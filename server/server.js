4dd3dfdbfa502dfdbe21d4e774928bdf7320db20
chungchau92 <chungchau92@gmail.com>
2023-03-09 22:20:38 +0700

    first commit

--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const { ApolloServer } = require("apollo-server-express");
+
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -13,8 +16,6 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
-
-app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
