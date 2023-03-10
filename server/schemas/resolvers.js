25b86977467858234acb539c97861b16096a492b
chungchau <chungchau92@gmail.com>
2023-03-10 17:27:25 +0700

    adding Query resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,1 +1,16 @@
-const {} = require("../models");
+const { User } = require("../models");
+
+const { signToken, authMiddleware } = require("../utils/auth");
+
+const resolvers = {
+    Query: {
+        me: async ( parent, args, context) => {
+            return User.findOne({})
+        }
+    },
+    Mutation: {
+        login: async ( req, res) => {
+
+        }
+    }
+}
