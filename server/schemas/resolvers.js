ffa4291697882b860573f5182d17c849612b211e
chungchau92 <chungchau92@gmail.com>
2023-03-10 20:39:29 +0700

    adding resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,16 +1,41 @@
-const { User } = require("../models");
+const { User, Book } = require("../models");
+const { AuthenticationError } = require('apollo-server-express');
+
 
 const { signToken, authMiddleware } = require("../utils/auth");
+
 
 const resolvers = {
     Query: {
         me: async ( parent, args, context) => {
-            return User.findOne({})
+            if(context.user) {
+                return User.findOne({_id: context.user._id })
+            }
+            throw new AuthenticationError("You need to be logged in!");
         }
     },
     Mutation: {
-        login: async ( req, res) => {
+        addUser: async ( parent, { username, password, email}) => {
+            const user = User.create({ username, password, email})
+            const token = signToken(user);
+            return { user, token};
+        },
+        login: async ( parent, { email, password}) => {
+            const user = await User.findOne({ email })
 
-        }
+            if(!user) {
+                throw new AuthenticationError('No user found with this email address');
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if(!correctPw) {
+                throw new AuthenticationError("Incorrect credentials")
+            }
+
+            const token = signToken(user);
+
+            return { token, user }
+        },
     }
 }
