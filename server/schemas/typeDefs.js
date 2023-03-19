86355c376c3a882d3d3be80375993f0fdbdf7554
chungchau92 <chungchau92@gmail.com>
2023-03-19 22:29:04 +0700

    fixing bug

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,20 +2,20 @@ const { gql } = require("apollo-server-express")
 
 const typeDefs = gql`
     type User {
-        _id: ID!
-        username: String!
-        password: String!
-        email: String!
-        bookCount: Int!
+        _id: ID
+        username: String
+        password: String
+        email: String
+        bookCount: Int
         savedBooks: [Book]
     }
     type Book {
-        bookId: String!
-        authors: [String]!
-        description: String!
-        title: String!
-        image: String!
-        link: String!
+        bookId: String
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
     }
     type Auth {
         token: ID!
