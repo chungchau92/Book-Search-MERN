70d7731a8c5ad6ef406df58f6dffeeff14adc6ef
chungchau <chungchau92@gmail.com>
2023-03-22 12:17:34 +0700

    fix add username mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -13,8 +13,8 @@ export const LOGIN_USER = gql`
 `;
 
 export const ADD_USER = gql`
-  mutation addUSer($name: String!, $email: String!, $password: String!) {
-    addUser(name: $name, email: $email, password: $password) {
+  mutation addUSer($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
       token
       user {
         _id
